657688d4447804dd0a48a3abf7cf4452d1eed2c0
season poon <seasonluke@gmail.com>
2023-06-29 02:36:53 +0800

    Implemented problem 21

--- 21-30/mergetwosortedlist/Source.cpp
+++ 21-30/mergetwosortedlist/Source.cpp
@@ -1,30 +1,65 @@
-
- struct ListNode {
-     int val;
-     ListNode *next;
-     ListNode() : val(0), next(nullptr) {}
-     ListNode(int x) : val(x), next(nullptr) {}
-     ListNode(int x, ListNode *next) : val(x), next(next) {}
- };
- 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         // trivial cases
         if ((list1 == NULL) && (list2 == NULL))
-        {
             return NULL;
-        }
 
-        ListNode* res = NULL;
-        ListNode* head = res;
+        if (list1 == NULL)
+            return list2;
+
+        if (list2 == NULL)
+            return list1;
+
+        ListNode* res = new ListNode();
+        ListNode* head = NULL;
+        ListNode* temp;
 
         // while 2 lists are not empty
-        // compare and append res
+        while ((list1 != NULL) && (list2 != NULL)) {
+            if (list1->val < list2->val) {
+                temp = new ListNode(list1->val);
+                list1 = list1->next;
+            }
+            else {
+                temp = new ListNode(list2->val);
+                list2 = list2->next;
+            }
+            if (head == NULL) {
+                res->val = temp->val;
+                head = res;
+            }
+            else {
+                head->next = temp;
+                head = head->next;
+            }
+        }
 
         // append rest in left if any
+        while ((list1 != NULL)) {
+            temp = new ListNode(list1->val);
+            list1 = list1->next;
+            head->next = temp;
+            head = head->next;
+        }
 
         // append rest in right if any
+        while ((list2 != NULL)) {
+            temp = new ListNode(list2->val);
+            list2 = list2->next;
+            head->next = temp;
+            head = head->next;
+        }
 
         return res;
     }
