dc6cedda1f9dd99ab570979efa94ce2732992f6d
Season <seasonluke@gmail.com>
2023-07-20 14:08:13 +0800

    Implemented problem 145

---
+++ 141-150/preordertraversal/postordertraversal/Source.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+void postorder(TreeNode* root, vector<int>& res) {
+    if (root == nullptr) return;
+
+    postorder(root->left, res);
+    postorder(root->right, res);
+    res.push_back(root->val);
+
+}
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> res;
+        postorder(root, res);
+        return res;
+    }
+};
