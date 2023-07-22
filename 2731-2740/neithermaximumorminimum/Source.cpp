427dec02932b6beed1fcbeef77a0073e896da4b4
Season <seasonluke@gmail.com>
2023-07-22 14:56:23 +0800

    Implemented problem 2733

--- 2731-2740/neithermaximumorminimum/Source.cpp
+++ 2731-2740/neithermaximumorminimum/Source.cpp
@@ -1,6 +1,10 @@
+// Only sorting contributes to time complexity.
+
 class Solution {
 public:
     int findNonMinOrMax(vector<int>& nums) {
-
+        if (nums.size() <= 2) return -1;
+        sort(nums.begin(), nums.end());
+        return nums[1];
     }
 };
