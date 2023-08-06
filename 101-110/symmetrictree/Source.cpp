32281e381da608105f06eaf587926beb9c030c3c
Season <seasonluke@gmail.com>
2023-08-06 13:19:57 +0800

    Implemented problem 101

---
+++ 101-110/symmetrictree/Source.cpp
@@ -0,0 +1,88 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+// beats 100% time, but very space expensive. Can be optimized.
+
+// O(n) symmetric check
+bool symmetric(vector<TreeNode* >& left_tn, vector<TreeNode* >& right_tn) {
+    for (int i = 0, j = right_tn.size() - 1; i < left_tn.size() && j >= 0; i++, j--) {
+        if (right_tn[i]->val == -150 && left_tn[j]->val != -150)
+            return false;
+        if (right_tn[i]->val != -150 && left_tn[j]->val == -150)
+            return false;
+        if (right_tn[i]->val != left_tn[j]->val) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool allnull(vector<TreeNode* >& left_tn, vector<TreeNode* >& right_tn) {
+    for (int i = 0, j = right_tn.size() - 1; i < left_tn.size() && j >= 0; i++, j--) {
+        if (left_tn[i]->val != -150 || right_tn[j]->val != -150) return false;
+    }
+    return true;
+}
+
+bool check(vector<TreeNode* >left_tn, vector<TreeNode* >right_tn) {
+    if (allnull(left_tn, right_tn))
+        return true;
+    if (!symmetric(left_tn, right_tn))
+        return false;
+
+    vector<TreeNode* > new_left;
+    for (int i = 0; i < left_tn.size(); i++) {
+        if (left_tn[i]->val != -150) {
+            if (left_tn[i]->left == nullptr)
+                new_left.push_back(new TreeNode(-150));
+            else
+                new_left.push_back(left_tn[i]->left);
+            if (left_tn[i]->right == nullptr)
+                new_left.push_back(new TreeNode(-150));
+            else
+                new_left.push_back(left_tn[i]->right);
+        }
+    }
+
+    vector<TreeNode* > new_right;
+    for (int i = 0; i < right_tn.size(); i++) {
+        if (right_tn[i]->val != -150) {
+            if (right_tn[i]->left == nullptr)
+                new_right.push_back(new TreeNode(-150));
+            else
+                new_right.push_back(right_tn[i]->left);
+            if (right_tn[i]->right == nullptr)
+                new_right.push_back(new TreeNode(-150));
+            else
+                new_right.push_back(right_tn[i]->right);
+        }
+    }
+
+    return check(new_left, new_right);
+}
+
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        vector<TreeNode* > left;
+        vector<TreeNode* > right;
+        if (root->left == nullptr)
+            left.push_back(new TreeNode(-150));
+        else
+            left.push_back(root->left);
+        if (root->right == nullptr)
+            right.push_back(new TreeNode(-150));
+        else
+            right.push_back(root->right);
+        return check(left, right);
+    }
+};
