aeabcc0c2a3663b05fc2ade57e853f705fa3525e
Season <seasonluke@gmail.com>
2023-08-09 16:06:12 +0800

    Implemented problem 242

---
+++ 241-250/validanagram/Source.cpp
@@ -0,0 +1,13 @@
+// O(sort) time
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length())
+            return false;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
+        return s == t;
+    }
+};
