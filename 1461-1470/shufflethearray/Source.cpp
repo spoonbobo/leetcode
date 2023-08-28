20bde5fee8d75e78d6c646fadd303add627bc0be
Season <seasonluke@gmail.com>
2023-08-28 08:47:21 +0800

    Implemented problem 1470

---
+++ 1461-1470/shufflethearray/Source.cpp
@@ -0,0 +1,14 @@
+// O(n) solution
+
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> res;
+        int k = 0;
+        while (k < n) {
+            res.push_back(nums[k]); res.push_back(nums[k + n]);
+            k++;
+        }
+        return res;
+    }
+};
