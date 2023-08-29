bcc8216b324d05e25329c0bd422a7322ec2d1b09
Season <seasonluke@gmail.com>
2023-08-29 18:59:20 +0800

    Implemented problem 511

---
+++ 511-520/gameplayanalysis1.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def game_analysis(activity: pd.DataFrame) -> pd.DataFrame:
+    ans = activity.sort_values(by=['event_date']).drop_duplicates(subset=['player_id'])[["player_id", "event_date"]].rename(columns={"event_date": "first_login"})
+
+    return ans
