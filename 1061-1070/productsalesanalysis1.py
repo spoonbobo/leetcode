10c4e0c05b238c1e109b74f928d011206639bcfc
Season <seasonluke@gmail.com>
2023-09-01 23:48:00 +0800

    Implemented problem 1068

---
+++ 1061-1070/productsalesanalysis1.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+def sales_analysis(sales: pd.DataFrame, product: pd.DataFrame) -> pd.DataFrame:
+    return pd.merge(sales, product, on="product_id")[["product_name", "year", "price"]]
