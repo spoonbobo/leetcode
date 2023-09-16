acbd7d312a121bde4dfc25eb1ce2648628552d02
Season <seasonluke@gmail.com>
2023-09-16 16:31:27 +0800

    Implemented problem 1290

---
+++ convertbinnumberinalinkedlisttointeger/Source.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+ // 2scan O(n) solution, beats 100% time
+
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        int res = 0;
+        int n = 0;
+        ListNode* filter = head;
+        while (filter->next != nullptr) {
+            n++; filter = filter->next;
+        }
+        while (head != nullptr) {
+            res += head->val * pow(2, n);
+            n--; head = head->next;
+        }
+        return res;
+    }
+};
