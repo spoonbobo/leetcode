15f55137fe81020bb75c91a88dfc7125798a399d
Season <seasonluke@gmail.com>
2023-10-04 11:43:15 +0800

    Implemented problem 1480

---
+++ 1471-1480/runningsumof1darray/Source.cpp
@@ -0,0 +1,11 @@
+// prefix sum, O(n) solution
+
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        for (int i = 1; i < nums.size(); i++) {
+            nums[i] += nums[i - 1];
+        }
+        return nums;
+    }
+};
