02d1aa1e0a13e2396862e23329ee16f46a067103
Season <seasonluke@gmail.com>
2023-10-16 14:54:28 +0800

    Implemented problem 1662

--- 1441-1450/counttripletsthatcanformtwoarraysofequalxor/Source.cpp
+++ 1441-1450/counttripletsthatcanformtwoarraysofequalxor/Source.cpp
@@ -1,7 +1,7 @@
 /*
 for any sub array A [i, k] with a size > 2,
 if one satisfies A[i]^A[i+1]^...^A[j-1]^A[j]^A[j+1]^... ^A[k] = 0 (^=XOR)
-Then A[i]^...A[j-1] AND A[j]...A[k] must be EQUAL based XOR property <- learnt
+Then A[i]^...A[j-1] AND A[j]^...^A[k] must be EQUAL based on XOR property <- learnt
 Given i<j<=k, we are select any j in range of [i+1, k] to satisfy A[i:j] == A[j:k]
 */
 
---
+++ 1661-1670/checkiftwostringarraysareequivalent/Source.cpp
@@ -0,0 +1,13 @@
+// O(s1Length+s2Length) time and space solution
+
+class Solution {
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        string s1 = "", s2 = "";
+        for (auto& c : word1)
+            s1 += c;
+        for (auto& c : word2)
+            s2 += c;
+        return s1 == s2;
+    }
+};
