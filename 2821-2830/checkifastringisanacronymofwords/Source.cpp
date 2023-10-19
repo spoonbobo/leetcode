7e9a631e7a7e7807551d7b1e74a374dcb3c0a1d7
Season <seasonluke@gmail.com>
2023-10-19 20:55:05 +0800

    Implemented problem 2828

---
+++ 2821-2830/checkifastringisanacronymofwords/Source.cpp
@@ -0,0 +1,13 @@
+// O(n) solution
+
+class Solution {
+public:
+    bool isAcronym(vector<string>& words, string s) {
+        if (words.size() != s.length())
+            return false;
+        for (int i = 0; i < s.size(); i++)
+            if (words[i][0] != s[i])
+                return false;
+        return true;
+    }
+};
