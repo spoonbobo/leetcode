a8bb2fd4e27c586b40c4e1d8a088068dad08cf6d
Season <seasonluke@gmail.com>
2024-03-13 14:37:16 +0800

    Learnt and implemented problem 1171

--- 1171-1180/removezerosumconsecutivenodesfromlinkedlist/Source.cpp
+++ 1171-1180/removezerosumconsecutivenodesfromlinkedlist/Source.cpp
@@ -9,23 +9,43 @@
  * };
  */
 
-// first attempt to problem, failed
-
 
 class Solution {
 public:
     ListNode* removeZeroSumSublists(ListNode* head) {
         unordered_map<int, ListNode*> m;
         ListNode* filter = head;
-        int prefix_sum = 0;
+        int prefix_sum = 0, tmp_sum;
         while (filter != nullptr) {
             prefix_sum += filter->val;
-            if (m[prefix_sum]) {
-                m[prefix_sum]->next = filter->next;
+            if (prefix_sum == 0) {
+                head = filter->next;
+                m.clear();
             }
-            m[prefix_sum] = filter;
+            if (m[prefix_sum]) {
+                ListNode* filter2 = m[prefix_sum];
+                tmp_sum = prefix_sum;
+                // eliminate until next node 
+                while (true) {
+                    tmp_sum += filter2->next->val;
+                    if (tmp_sum == prefix_sum) {
+                        m[prefix_sum]->next = filter->next;
+                        break;
+                    }
+                    m[tmp_sum] = 0;
+                    filter2 = filter2->next;
+                }
+
+            }
+            else
+                m[prefix_sum] = filter;
+
             filter = filter->next;
         }
+
+        if (!prefix_sum)
+            return nullptr;
+
         return head;
     }
 };
