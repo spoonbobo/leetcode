80bd3fe2cfb52fce436d7f62e73ec2f6352ab71a
Season <seasonluke@gmail.com>
2024-03-17 22:06:42 +0800

    Learnt and implemented problem 238

---
+++ 231-240/productofarrayexceptself/Source.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> left(n, 0);
+        vector<int> right(n, 0);
+        left[0] = 1; right[n - 1] = 1;
+        for (int i = 1; i < n; i++) {
+            left[i] = nums[i - 1] * left[i - 1];
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            right[i] = nums[i + 1] * right[i + 1];
+        }
+
+        for (int i = 0; i < n; i++) {
+            left[i] *= right[i];
+        }
+
+        return left;
+
+    }
+};
