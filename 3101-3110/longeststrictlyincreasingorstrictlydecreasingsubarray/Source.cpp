37da9630dde96066b1ff182742d0e3501e80591e
Season <seasonluke@gmail.com>
2024-04-07 23:30:49 +0800

    Implemented problem 3105

---
+++ 3101-3110/longeststrictlyincreasingorstrictlydecreasingsubarray/Source.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int longestMonotonicSubarray(vector<int>& nums) {
+        int ans = 1;
+        int i, j, n;
+        i = 0; j = 1; n = nums.size();
+        while (i < n && j < n) {
+            if (!(nums[j] > nums[j - 1])) {
+                i = j;
+                j = i + 1;
+            }
+            else {
+                ans = max(ans, j - i + 1);
+                j++;
+            }
+        }
+
+        i = 0; j = 1;
+        while (i < n && j < n) {
+            if (!(nums[j] < nums[j - 1])) {
+                i = j;
+                j = i + 1;
+            }
+            else {
+                ans = max(ans, j - i + 1);
+                j++;
+            }
+        }
+
+        return ans;
+    }
+};
